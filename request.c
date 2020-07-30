eafa10741052d4043fcd333e635c29e0f5ce1048
Sekhar Pariga <sekharpariga@gmail.com>
2020-07-30 09:37:11 -0400

    tested Working custom strtok

--- request.c
+++ request.c
@@ -1,39 +1,11 @@
 #include "request.h"
 
-struct parsedata * clientrequest_old(char *data)
-{
-	struct parsedata *pdata = (struct parsedata *) malloc(sizeof(struct parsedata));
-	int len = 0;
-	char *token = strtok(data, " ");
-
-	if(token != NULL)
-	{
-		len = strlen(token);
-		pdata->cmd = (char *) malloc((len+1) * sizeof(char));
-		strncpy( pdata->cmd, token, len);
-	}
-	else
-		pdata->cmd = NULL;
-
-	token = strtok(NULL, " ");
-	
-	if(token != NULL)
-	{
-		len = strlen(token);
-		pdata->arg = (char *) malloc((len+1) * sizeof(char));
-		strncpy(pdata->arg, token, len);
-	}
-	else
-		pdata->arg = NULL;
-	
-	return pdata;
-}
-
 struct parsedata * clientrequest(char *data, int msgsize)
 {
 	int cmdstart = 0, argstart = 0, cmdlen = 0, arglen = 0, index = 0;
 	char *temp = data, *cmd = NULL, *arg = NULL;
 	struct parsedata *pdata = (struct parsedata *) malloc(sizeof(struct parsedata));
+	printf("data:fun:clientrequest:%s-\n", data);
 
 	while(*data == ' '  && index < msgsize) 
 	{
@@ -64,15 +36,13 @@ struct parsedata * clientrequest(char *data, int msgsize)
 		index++;
 	}
 
-	if(cmdlen > 0 && cmdlen < msgsize)
-		cmd = (char *) malloc(sizeof(char) * (cmdlen + 1));
-	if(arglen > 0 && arglen < msgsize)
-		arg = (char *) malloc(sizeof(char) * (arglen + 1));
+	if(cmdlen > 0)
+		cmd = strndup(temp + cmdstart, cmdlen);
+	if(arglen > 0)
+		arg = strndup(temp + argstart, arglen);
 
-	strncpy(cmd, temp + cmdstart, cmdlen);
-	cmd[cmdlen] = 0;
-	strncpy(arg, temp + argstart, arglen);
-	arg[arglen] = 0;
+	if(cmdstart == argstart && cmdlen > 0 && arglen == 0)
+		arg = NULL;
 
 	pdata->cmd = cmd;
 	pdata->arg = arg;
