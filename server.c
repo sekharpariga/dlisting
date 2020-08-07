db5f83a9b953c2e1b8fe707f7a33226dbcbb6406
Sekhar Pariga <sekharpariga@gmail.com>
2020-08-07 13:13:08 -0400

    updated server and removed README.md file

--- server.c
+++ server.c
@@ -5,17 +5,22 @@ pthread_mutex_t lock;
 int main()
 {
 	pthread_t thread_id[THREADPOOL];
-	int serverfd, clientfd;
+
 	struct sockaddr_in address;
-	int addrlen = sizeof(address), opt = 1;
+	char *mainpwd = malloc(sizeof(char) * PATH_MAX);
+
+	int addrlen = sizeof(address);
+	int opt = 1;
+	int serverfd;
+	int clientfd;
+	int *pclient;
 
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
-	int *pclient;
-	char *mainpwd = malloc(sizeof(char) * PATH_MAX);
+
 	if(getcwd(mainpwd, PATH_MAX) == NULL)
-		exit(8);
+		exit(1);
 
 	if(signal(SIGINT, signal_handler) == SIG_ERR)		//ctrl-c
 		perror("can't catch SIGTERM\n");
@@ -24,24 +29,21 @@ int main()
 		perror("can't catch SIGTSTP\n");
 	
 	if(pthread_mutex_init(&lock, NULL) != 0)
-	{
-		perror("mutex lock init failed!\n");
-		exit(1);
-	}
+		exit(2);
 
 	serverfd = socket(AF_INET,SOCK_STREAM, 0);
 
 	if(serverfd == 0)
-		exit(2);
+		exit(3);
 	
 	if(setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT | TCP_NODELAY, &opt, sizeof(opt)))
-	       exit(3);
+	       exit(4);
 
 	if(bind(serverfd,(struct sockaddr *) &address, addrlen) != 0)
-		exit(4);
+		exit(5);
 
 	if(listen(serverfd, BACKLOG) != 0)
-		exit(5);
+		exit(6);
 
 	for(int i = 0; i < THREADPOOL; i++)
 		pthread_create(&(thread_id[i]), NULL, &threadhandle, NULL);
